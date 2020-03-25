74b4491d2e352867bee55b48443d1c759ef978a8
Ivan Morett <luisivanmorett@gmail.com>
2020-03-25 14:12:55 -0600

    auth0 authentication using jwt on server side

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import express from 'express';
+import { AuthenticationMiddleware } from './common/authentication.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(new AuthenticationMiddleware().use)
   await app.listen(3000);
 }
 bootstrap();
